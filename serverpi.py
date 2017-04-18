ae5272c36b8d85b831a0d79c361f5c213911472d
hbisyir2 <hbisyir2@vt.edu>
2017-04-18 11:56:49 -0400

    added some lines

--- serverpi.py
+++ serverpi.py
@@ -1,7 +1,18 @@
-# For server pi
-#while True:
-#	if GPIO.input(7):
-#		print('Fire detected!')
+import RPi.GPIO as GPIO
+import time
+
+while True:
+	if GPIO.input(7):
+		print('Fire detected!')
 		# send message to thermometers and maps via sockets
-#		break
-#	time.sleep(0.5)
+		break
+	time.sleep(0.5)
+
+while True:
+	time.sleep(10)
+	# get tuple from both thermometers
+	# unpickle both objects
+	# compile into JSON object
+	# store into MongoDB
+	# pickle JSON
+	# send to map pi via socket
