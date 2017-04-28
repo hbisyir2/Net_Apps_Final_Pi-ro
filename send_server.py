d2e934c9150af0d29a82d2fbe66514bde34a2502
Andrew Bryant <andrewpb@vt.edu>
2017-04-28 13:42:19 -0400

    Slight change

--- send_server.py
+++ send_server.py
@@ -2,11 +2,12 @@ import sys
 import pickle
 import json
 import socket
+import time
 
-sock1_connect = ('10.0.0.78', 8000)
+sock1_connect = ('172.29.124.24', 8000)
 sock1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-sock2_connect = ('10.0.0.78', 8001)
+sock2_connect = ('172.29.124.24', 8001)
 sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
@@ -21,10 +22,6 @@ def server_pickle(temp1, time1, temp2, time2):
 #Unpickles JSON object, and returns contents as a dictionary
 def server_unpickle(pickled_data):
     return json.loads(pickle.loads(pickled_data))
-    
-
-send1 = "Hello Jason"
-send2 = "More data"
 
 def send_data1(obj1):
 	try:
@@ -52,7 +49,23 @@ def send_data2(obj2):
 		print 'Closing socket 2'
 		sock2.close()
 
+dict1 = {"time": "01:25:00", "temp": 22.1}
+dict2 = {"time": "01:25:01", "temp": 22.2}
+dict3 = {"time": "01:25:02", "temp": 22.3}
+dict4 = {"time": "01:25:03", "temp": 22.4}
 
-send_data2(send2)
-send_data1(send1)
+
+try:
+
+		pick_obj1 = server_pickle(dict1["temp"], dict1["time"], dict2["temp"], dict2["time"])
+		send_data1(pick_obj1)
+		time.sleep(1)
+		pick_obj2 = server_pickle(dict3["temp"], dict3["time"], dict4["temp"], dict4["time"])
+		send_data2(pick_obj2)
+		time.sleep(1)
+	
+finally:
+	print "close"
+
+
 
