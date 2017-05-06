823aad268950712b6d60b082869c23788d69c3b9
Andrew Bryant <andrewpb@vt.edu>
2017-05-06 16:47:30 -0400

    Minor fix

--- thermometer_1.py
+++ thermometer_1.py
@@ -28,6 +28,7 @@ def SendMessage(address, port, temp, time):
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect((address, port))
 	s.send(sentPickle)
+	s.close()
 
 def read_temp_raw():
 	f = open(device_file, 'r')
